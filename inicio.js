48a1c14e974fb5f57815064bb9c28d7b98dd5f8d
RyuutaroShi <therodzzmp@gmail.com>
2024-03-13 11:23:48 -0500

    -

--- inicio.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    //menú vertical
-      $(".navbar-toggler").click(function(e) {
-          e.preventDefault();
-          $("#navbarNav").toggleClass("active");
-      });
-  
-  });
